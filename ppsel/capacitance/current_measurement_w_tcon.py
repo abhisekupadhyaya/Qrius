d02463dfaa4c2c9791cb0cf7b312788f78f77cd4
gitansh95 <gitansh95@gmail.com>
2017-06-30 14:36:22 +0530

    5 values of voltages are now set, with multiple measurements for each value

--- ppsel/capacitance/current_measurement_w_tcon.py
+++ ppsel/capacitance/current_measurement_w_tcon.py
@@ -21,18 +21,19 @@ f.close()
 
 def set_DC_voltage (xsmu_driver, value):
 
-	mode       = SOURCE_MODE_VS
-	autorange  = AUTORANGE_ON
-	range      = VS_RANGE_10V
-	
-	xsmu_driver.setSourceParameters (mode, autorange, range, value)
+    mode       = SOURCE_MODE_VS
+    autorange  = AUTORANGE_ON
+    range      = VS_RANGE_10V
 
-def measure_current(xsmu_driver, iterations):
+    xsmu_driver.setSourceParameters (mode, autorange, range, value)
+
+def measure_current(xsmu_driver, xtcon_driver, iterations):
     
     f = open("I-Data.txt", "a")
     for index in range(iterations):
         current = xsmu_driver.CM_getReading( filterLength = 1 )
-        f.write(str(current) + '\n')
+        temperature = xtcon_driver.getSampleTemperature()
+        f.write(str(current) + "," + str(temperature) + '\n')
     f.close() 	
    
 def stabilize_temp (xtcon_driver, tolerance):
@@ -48,7 +49,7 @@ def stabilize_temp (xtcon_driver, tolerance):
             continue
         else :
             fluctuation = max(history[-100:-1]) - min(history[-100:-1])
-            print ("Max : ", max(history[-100:-1]), "\tMin : ", min(history[-100:-1]))
+            print "Max : " + str (max(history[-100:-1])) + "\tMin : " + str(min(history[-100:-1]))
             
             if (np.abs(fluctuation) < tolerance):
                 print ("Fluctuation : " + str(fluctuation))
@@ -59,29 +60,38 @@ def stabilize_temp (xtcon_driver, tolerance):
 
 def main():
 	
-	xsmu_driver = xsmu.Driver()
 	xtcon_driver = tcon.Driver()
-	
-	xsmu_devices = xsmu_driver.scan()
-	xsmu_driver.open(xsmu_devices[0])
-	
-	xtcon_devices = xtcon_driver.scan()
+        xtcon_devices = xtcon_driver.scan()
 	xtcon_driver.open(xtcon_devices[0])
 	
-	DC_amplitude   = float(raw_input ("Enter DC Voltage (V) : "))    # V
 	setpoint       = float(raw_input ("Enter isothermal setpoint (K) :"))
 	tolerance      = float(raw_input ("Tempereature Tolerance (over 10 successive readings) (K) : "))
-	iterations     = int  (raw_input ("Iterations : "))
 	
 	xtcon_driver.setIsothermalSetpoint (setpoint)
 	print ("Starting Isothermal Run.. \n")
 	xtcon_driver.startIsothermalControl()
 	
 	stabilize_temp (xtcon_driver, tolerance)
+	# Stabilizes the temperature and waits for user to take an IV run from Qrius GUI
+	print ("Press Enter when IV from Qrius GUI is complete")
 	
-	print ("Setting DC Voltage.. \n")
-	set_DC_voltage (xsmu_driver, DC_amplitude)
-	measure_current(xsmu_driver, iterations)
+	response = raw_input("Press y to continue? : y \n")
+	while (response != 'y'):
+            response = raw_input("Press y to continue? : y/n \n")
+
+	xsmu_driver = xsmu.Driver()
+	xsmu_devices = xsmu_driver.scan()
+	xsmu_driver.open(xsmu_devices[0])
+	
+	for i in range(5):
+	
+            DC_amplitude   = float(raw_input ("Enter DC Voltage (V) : "))    # V
+            iterations     = int  (raw_input ("Iterations : "))
+	
+            print ("Setting DC Voltage.. \n")
+	
+            set_DC_voltage (xsmu_driver, DC_amplitude)
+            measure_current(xsmu_driver, xtcon_driver, iterations)
 	
 	xtcon_driver.stopIsothermalControl()
 	set_DC_voltage (xsmu_driver, 0.0)
