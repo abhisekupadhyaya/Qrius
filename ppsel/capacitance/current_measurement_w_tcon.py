54365ad8ba8a4fdc113ef5a9a1f8da25c799af25
vishnu2709 <vishnu.raman@gmail.com>
2017-06-30 14:08:51 +0530

    Data aquired will now be written to file

--- ppsel/capacitance/current_measurement_w_tcon.py
+++ ppsel/capacitance/current_measurement_w_tcon.py
@@ -6,11 +6,13 @@ sys.path.insert(0,"../../apps")
 sys.path.insert(0,"../../apps/widgets")
 sys.path.insert(0,"../../lib")
 
-import math
 import xsmu
 import tcon
 import numpy as np
 from XSMU_Constants import *
+
+f = open("I-Data.txt", "w")
+f.close()
 
 # Set voltage source to some value
 # Call function to measure current
@@ -25,11 +27,13 @@ def set_DC_voltage (xsmu_driver, value):
 	
 	xsmu_driver.setSourceParameters (mode, autorange, range, value)
 
-def measure_current(xsmu_driver):
+def measure_current(xsmu_driver, iterations):
     
-    for index in range(10):
+    f = open("I-Data.txt", "a")
+    for index in range(iterations):
         current = xsmu_driver.CM_getReading( filterLength = 1 )
-        print ("The value of current is " + str(current) + " and the time is " + str(time.strftime('%H:%M:%S')) + '\n') 	
+        f.write(str(current) + '\n')
+    f.close() 	
    
 def stabilize_temp (xtcon_driver, tolerance):
     
@@ -38,7 +42,6 @@ def stabilize_temp (xtcon_driver, tolerance):
     print ("Stabilizing .. \n")
     
     while True :
-        print ("Loop entered")
         history.append(xtcon_driver.getSampleTemperature())
         
         if (len(history)<100):
@@ -68,6 +71,7 @@ def main():
 	DC_amplitude   = float(raw_input ("Enter DC Voltage (V) : "))    # V
 	setpoint       = float(raw_input ("Enter isothermal setpoint (K) :"))
 	tolerance      = float(raw_input ("Tempereature Tolerance (over 10 successive readings) (K) : "))
+	iterations     = int  (raw_input ("Iterations : "))
 	
 	xtcon_driver.setIsothermalSetpoint (setpoint)
 	print ("Starting Isothermal Run.. \n")
@@ -77,7 +81,7 @@ def main():
 	
 	print ("Setting DC Voltage.. \n")
 	set_DC_voltage (xsmu_driver, DC_amplitude)
-	measure_current(xsmu_driver)
+	measure_current(xsmu_driver, iterations)
 	
 	xtcon_driver.stopIsothermalControl()
 	set_DC_voltage (xsmu_driver, 0.0)
